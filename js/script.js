563f68b2249d096432fe918c18fa506a37417c62
NICOGATT <130109204+NICOGATT@users.noreply.github.com>
2024-06-29 16:33:41 -0300

    Cambiando la posicion del carrito respecto a la pagina, y solucione el problema del nav

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
+// Añadimos un evento que hace que se se escucha el click 
 document.addEventListener("DOMContentLoaded", function(){
+    // Aca se agrega dos constantes uno para el menu hamburguesa y otro para el nav
     const toggleBtn = document.getElementById("menu__hamburguer");
     const nav = document.querySelector("nav");
     toggleBtn.addEventListener("click", function() {
@@ -22,3 +24,36 @@ document.addEventListener("DOMContentLoaded", function(){
        }
     })
 })
+
+//Cambiando la position del carrito
+document.addEventListener("DOMContentLoaded", () => {
+    const inicio = document.getElementById("inicio");
+    const carrito = document.getElementById("carrito");
+    let isAbsolute = false; 
+    function tooglePosition() {
+        const pathname = window.location.pathname
+        if(pathname === "/" || pathname.endsWith("index.html")){
+            if(isAbsolute) {
+                carrito.style.position = "relative";
+                carrito.style.top = "0"
+                carrito.style.right = "0"
+            } else {
+                carrito.style.position = "absolute"
+                carrito.style.top = "20%"
+                carrito.style.right = "15%"
+            }
+        }
+        isAbsolute = !isAbsolute
+    }
+    //Agregamos el evento click
+    inicio.addEventListener("click", tooglePosition())
+
+    const pathname = window.location.pathname
+    if (pathname !== "/" && !pathname.endsWith("index.html")) {
+        carrito.style.position = "absolute"
+        carrito.style.top = "28%"
+        carrito.style.right = "1%"
+    }
+})
+
+// console.log(window.location.pathname)
